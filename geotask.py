62d63291ce4777735a26d769397bd675618514b7
Jniem <jniemczura@outlook.com>
2020-12-01 20:19:29 +0100

    Add prototype code that allows creating file for case 1)

--- geotask.py
+++ geotask.py
@@ -3,13 +3,14 @@ and save it in separate .txt files for better readability """
 
 from pathlib import Path
 import os
+import re
 
 def inputFilePath():
     """ Asks user for input and validates it. Returns validated absolute path of .txt file """
     inputPath = input(r'Please paste absolute file path: (e.g. C:\Program Files\Stuff\File.txt)') # Ask user for complete file path.
     savedPath = Path(inputPath) # Save given path in variable using Path (for compatibilty).
     while True: # Loop validating given path, returning savedPath if valid.
-        if os.path.isabs(inputPath) and savedPath.exists() and savedPath.is_file():
+        if os.path.isabs(inputPath) and savedPath.exists() and savedPath.is_file():  # try/except better?
             return savedPath
         else:
             print('Given path was not an absolute path or non existent one.')
@@ -20,6 +21,38 @@ def openFile(Path):
     usedFile = open(Path)
     usedFileLists = usedFile.readlines()
     return usedFileLists
+
+def createSimplifiedFiles(Path, listOfStrings):
+    """ Reads through a .txt file looking for 'Input data format: Nr	X	Y	H'
+    Create output .txt files based on input .txt data:
+    1) Consisting of height measured (IS11.H.09 or similar format - ".H." matching pattern) in format:
+    H 	X	Y	H
+
+    2) Consisting of all points measured (both IS11.H.09 and IS11.O.17 - ".H." and ".O." matching pattern) in format:
+    X	Y	H
+
+    4) Consisting of names of points measured (both IS11.H.09 and IS11.O.17 - ".H." and ".O." matching pattern) in format:
+    Nr	X	Y	H """
+
+    """ Regex objects for searching through files (to be simplified) """
+    heightMeasuredRegex = re.compile(r'IS11\.H\.\d+ (\d+\.\d+) (\d+\.\d+) (\d+\.\d+)')
+    allMeasuredRegex = re.compile(r'(IS11\.H\.\d+|IS11\.O\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+\.\d+)')
+
+    
+    heightMeasured = open('HeightMeasured.txt', 'w')
+    for line in listOfStrings:
+        matchingObjects = heightMeasuredRegex.findall(line)
+        print(matchingObjects)
+        if matchingObjects != []:
+            X = matchingObjects[0][0]
+            Y = matchingObjects[0][1]
+            H = matchingObjects[0][2]
+            heightMeasured.write(H + ' ' + X + ' ' + Y + ' ' + H +'\n')
+    heightMeasured.close()
+
+userPath = inputFilePath()
+textList = openFile(userPath)
+createSimplifiedFiles(userPath, textList)
 
 
 
